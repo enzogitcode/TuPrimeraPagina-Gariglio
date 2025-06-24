b456e8d511485b0d14058677e65eb53efcba88d0
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-06-24 20:30:57 -0300

    edición del footer

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [path('', views.index, name='index'),
                path('students', views.students_list, name='students_list'),
+               path('teachers', views.teachers, name='teachers_list'),
                path('papers', views.papers_list, name='papers_list')
                ]
