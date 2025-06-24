6693e367818024b1fad9c43ff77f0c8ab50a00b0
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-06-24 03:08:27 -0300

    primer modelo renderizado de index html

--- AppBlog/models.py
+++ AppBlog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User():
+    pass
+class Student():
+    pass
+class Teacher():
+    pass
+class Article():
+    pass
+class Paper():
+    pass
