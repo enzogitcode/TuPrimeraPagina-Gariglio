005b10b2c2fb3f106ece0822fadf7e596f415173
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-06-25 01:11:34 -0300

    editando los templates, models y urls

--- AppBlog/views.py
+++ AppBlog/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'AppBlog/index.html')
-def teachers(request):
+def teachers_list(request):
     return render(request, 'AppBlog/teachers_list.html')
 
 def students_list(request):
     return render(request, 'AppBlog/students_list.html')
 def papers_list(request):
     return render(request, 'AppBlog/papers_list.html')
-def articles(request):
+def articles_list(request):
     return render(request, 'AppBlog/articles_list.html')
